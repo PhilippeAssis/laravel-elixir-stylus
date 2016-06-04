39d2dbccd8c2d2750827c18e517b007fc2a30877
Philippe Assis <assis@philippeassis.com>
2016-06-04 06:56:11 -0500

    Update for laravel-elixir 5.0.0
    laravel-elixir/dist/tasks/CssTask does not exist in laravel-elixir 5.0.0

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-var CssTask = require('laravel-elixir/dist/tasks/CssTask').default;
+'use strict';
+
+var _Css = require('../laravel-elixir/dist/tasks/shared/Css');
+
+var _Css2 = _interopRequireDefault(_Css);
+
+var _laravelElixir = require('laravel-elixir');
+
+var _laravelElixir2 = _interopRequireDefault(_laravelElixir);
+
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {default: obj};
+}
+
+var config = _laravelElixir2.default.config;
 
 /*
  |----------------------------------------------------------------
@@ -6,12 +20,12 @@ var CssTask = require('laravel-elixir/dist/tasks/CssTask').default;
  |----------------------------------------------------------------
  |
  | This task will compile your Stylus, including minification and
- | and auto-prefixing. Additionally it supports any postStylus
- | plugins that you want to include with your compilation.
+ | and auto-prefixing. Stylus is one of the CSS pre-precessors
+ | supported by Elixir, along with the Less CSS processor.
  |
  */
 
-Elixir.config.css.stylus = {
+_laravelElixir2.default.config.css.stylus = {
     folder: 'stylus',
     search: '/**/*.styl',
     plugin: require('gulp-stylus'),
@@ -22,21 +36,32 @@ Elixir.config.css.stylus = {
     }
 };
 
+var gulpTask = function gulpTask(src, output, options) {
+    var paths = prepGulpPaths(src, output);
 
-Elixir.extend('stylus', function(src, output, options) {
-    new CssTask('stylus', getPaths(src, output), options || {});
+    new _laravelElixir2.default.Task('stylus', function () {
+        return (0, _Css2.default)({
+            name: 'Stylus',
+            compiler: require('gulp-stylus'),
+            src: paths.src,
+            output: paths.output,
+            task: this,
+            pluginOptions: options || config.css.stylus.pluginOptions
+        });
+    }).watch(paths.src.baseDir + '/**/*.styl').ignore(paths.output.path);
+};
+
+_laravelElixir2.default.extend('stylus', function () {
+    gulpTask.apply(this, arguments);
 });
-
 
 /**
  * Prep the Gulp src and output paths.
  *
- * @param  {string|array} src
+ * @param  {string|Array} src
  * @param  {string|null}  output
- * @return {object}
+ * @return {GulpPaths}
  */
-var getPaths = function(src, output) {
-    return new Elixir.GulpPaths()
-        .src(src, Elixir.config.get('assets.css.stylus.folder'))
-        .output(output || Elixir.config.get('public.css.outputFolder'), 'app.css');
+var prepGulpPaths = function prepGulpPaths(src, output) {
+    return new _laravelElixir2.default.GulpPaths().src(src, config.get('assets.css.stylus.folder')).output(output || config.get('public.css.outputFolder'), 'app.css');
 };
